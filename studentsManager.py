52b3a0c8d39910b9d1106fd34ae9cad71d2afcf4
mohan-personal <mohan-personal@Mohans-MacBook-Pro.local>
2022-11-03 14:50:45 +0530

    student data - get and post method

---
+++ studentsManager.py
@@ -0,0 +1,38 @@
+# importing Flask and other modules
+from flask import Flask, request, render_template
+import uuid
+ 
+# Flask constructor
+app = Flask(__name__)  
+ 
+
+
+students = []
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/students', methods =["POST"])
+def post():
+    data = request.get_json()
+    data['id']= uuid.uuid1()
+    students.append(data)
+    return "",201
+
+@app.route("/students",methods = ["GET"])
+def get():
+    return students , 200
+
+# @app.route("/students",methods= ["PUT"])
+# def put():
+    
+
+
+
+# @app.route('/students', methods = ["GET"])
+# def get():
+#     return "",200
+
+# @app.route('/students', methods = ["PUT"])
+# def put():
+
+if __name__=='__main__':
+    app.run()
