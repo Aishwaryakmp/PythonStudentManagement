c657e399ec159664896058a468260daf331a62a6
mohan-personal <mohan-personal@Mohans-MacBook-Pro.local>
2022-11-04 12:56:33 +0530

    added delete funtion

--- demo.py
+++ demo.py
@@ -10,9 +10,9 @@ students = []
 # which URL is associated function
 @app.route('/students', methods =["POST"])
 def post():
-   data = request.get_json()
-   data['id']= uuid.uuid4()
-   students.append(data)
+   data = request.get_json() # getting the json data from the postman 
+   data['id']= uuid.uuid4() #used to auto generate id 
+   students.append(data) #storing the datas received from json in students
    return "",201
 
 @app.route("/students",methods = ["GET"])
@@ -23,14 +23,18 @@ def get():
 def put(studentId):
    args = studentId
 #  queryParams = request.args.get("test")
-   data=request.get_json()
-   for i in range (0, len(students)):    
+   data=request.get_json() # getting json data from postman 
+   for i in range (0, len(students)):    # for loop for checking if the id given and comparing ids are same 
       if studentId == str(students[i]["id"]):
              students[i]['name'] = data['name']
-   return students, 200
-    
-    
-    
-   return "", 200  
+   return "", 200
+
+@app.route("/students/<studentId>",methods=["DELETE"])
+def delete(studentId):
+       args=studentId
+       for i in range (0, len(students)):
+              if studentId == str(students[i]["id"]):
+                  students.pop(i)
+       return "",202  
 if __name__=='__main__':
    app.run()
