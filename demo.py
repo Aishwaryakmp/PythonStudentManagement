678091521ea7b7c6d7370795f28d88bf7b94aac5
Aishwarya <libaishwarya@gmail.com>
2022-11-22 20:38:00 +0530

    added changes in code which helps to add new name which is requested from postman

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 # importing Flask and other modules
 from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 import uuid
 import json
 import collections
@@ -9,12 +10,12 @@ from sqlalchemy import table
 
 # Flask constructor
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'demo'
 mysql = MySQL(app)
-
 
 students = []
 # jsonStr = json.dumps(students)
@@ -22,14 +23,23 @@ students = []
 # A decorator used to tell the application
 # which URL is associated function
 
-
 @app.route('/students', methods=["POST"])
 def post():
+   
 	data = request.get_json()  # getting the json data from the postman
-	data['id'] = uuid.uuid4()  # used to auto generate id
-	students.append(data)  # storing the datas received from json in students
-	return "", 201
+ 
+	college_name = data["collegeName"]
+	dept_name = data["deptName"]
+	cur2 = mysql.connection.cursor()
+	cur2.execute("select cd.id , c.college_name , d.name from college_department cd join college c on c.id = cd.college_id join department d on d.id = cd.dept_id where c.college_name=\"{0}\" and d.name=\"{1}\"".format(college_name, dept_name))
+	rv = cur2.fetchone()
+	dept_id = rv[0]
 
+	k = "insert into student (first_name, dept_id) values(\"{0}\",{1})".format(data['name'], dept_id)
+	cur2.execute("insert into student (first_name, dept_id) values(\"{0}\",{1})".format(data['name'], dept_id))
+	mysql.connection.commit()
+	cur2.close()
+	return "", 201	
 
 @app.route("/students", methods=["GET"]) # list all std names connected to db
 def list():
@@ -61,9 +71,9 @@ def get(studentId):
 	else:
 		return "", 404
 
-
 @app.route("/students/<studentId>", methods=["PUT"])
 def put(studentId):
+    
 	args = studentId
 #  queryParams = request.args.get("test")
 	data = request.get_json()  # getting json data from postman
@@ -72,7 +82,6 @@ def put(studentId):
 		if studentId == str(students[i]["id"]):
 			students[i]['name'] = data['name']
 	return "", 200
-
 
 # @app.route("/students/<studentId>", methods=["DELETE"]) # normal way to delete a data when not connected to db
 # def delete(studentId):
@@ -92,5 +101,3 @@ def delete(studentId):
    
 if __name__ == '__main__':
 	app.run()
-
-
